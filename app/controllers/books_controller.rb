214596c1f81360d5da5ab7d9e645331ae5c244f3
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-04 04:57:41 +0000

    [Add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,20 @@ class BooksController < ApplicationController
   end
   
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to book_path(book.id)
+   @book = Book.new(book_params)
+   @books = Book.all
+   if @book.save
+     flash[:success]="successfully created book！"
+     redirect_to book_path(@book.id)
+   else
+    render :index
+   end
   end
+  
   
   def index
    @books = Book.all
+   @book = Book.new
   end
 
   def show
@@ -18,15 +25,32 @@ class BooksController < ApplicationController
   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+   @book = Book.find(params[:id])
+   
+   if @book.update(book_params)
+    flash[:success]="successfully updated book！"
+    redirect_to book_path(@book.id)
+   else
+    render :edit
+   end
+   
   end
   
   def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   flash[:success]="Book was successfully destroyed！"
+   redirect_to '/books'
   end
   
   private
- def book_params
-  params.require(:book).permit(:title,:body)
- end
-
+   def book_params
+    params.require(:book).permit(:title,:body)
+   end
+ 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources:books
- post 'books' => 'books#create'
+  # post 'books' => 'books#create'
  root :to => 'homes#top'
 end
