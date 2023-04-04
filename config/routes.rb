9cd3827ca755501ba2ca66aa38316a452a7892fd
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-04 12:34:02 +0000

    [Finish]3-13

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources:books
   # post 'books' => 'books#create'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
  root :to => 'homes#top'
 end
